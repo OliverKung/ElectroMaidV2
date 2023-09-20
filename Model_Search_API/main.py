8bb814700e358049996b58d6cc5ea5a1b5ed19b9
OliverKung <43801635+OliverKung@users.noreply.github.com>
2023-09-20 22:17:50 +0800

    Fist Pushed
    First Add Code

---
+++ Model_Search_API/main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
+option = webdriver.ChromeOptions()
+option.add_argument('--headless')
+option.add_experimental_option('excludeSwitches',['enable-logging'])
+url = "https://www.semiee.com/search?searchModel="
+url_download="https://www.semiee.com/"
+model = input()
+driver = webdriver.Chrome(options=option)
+res = driver.get(url+model)
+html = driver.page_source
+print("get HTML success!")
+driver.quit()
+soup = BeautifulSoup(html,'html.parser')
+search_result = soup.find_all('div', class_="result-one")[0]
+model_id = search_result.get('data-modelid')
+# print(search_result)
+print(str(search_result.find_all("p")[1]).replace("<p>","").replace("</p>",""))
+print("----------------------------------------------------------------------------------------------")
+
+driver = webdriver.Chrome(options=option)
+res = driver.get(url_download+model_id+".html")
+html = driver.page_source
+driver.quit()
+soup = BeautifulSoup(html,'html.parser')
+link = soup.find_all('li',class_="openFile")[0].get("data-href")
+os.system("wget.exe -O "+model+".pdf -P ./Download_PDF/ "+link)
